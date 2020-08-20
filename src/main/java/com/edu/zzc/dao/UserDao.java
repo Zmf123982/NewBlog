051fb28967a97adcaeed78e0dbd801edd53315dd
EDZ <tome>
2020-08-20 17:29:20 +0800

    新增加API方法:通过名字查询用户信息

--- src/main/java/com/edu/zzc/dao/UserDao.java
+++ src/main/java/com/edu/zzc/dao/UserDao.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface UserDao {
     @Select("select * from cn_user")
     List<User> findAll();
+    @Select("select * from cn_user where cn_user_name = #{cn_user_name}")
+    User findByName(String cn_user_name);
 }
