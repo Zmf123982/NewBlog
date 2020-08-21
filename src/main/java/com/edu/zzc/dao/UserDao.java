e32498b8b1afd13bafbfaf1eda1fd828cd3c8ff6
EDZ <tome>
2020-08-21 17:20:15 +0800

    第一天实现了登录注册的前后分离

--- src/main/java/com/edu/zzc/dao/UserDao.java
+++ src/main/java/com/edu/zzc/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.edu.zzc.dao;
 
 import com.edu.zzc.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface UserDao {
     List<User> findAll();
     @Select("select * from cn_user where cn_user_name = #{cn_user_name}")
     User findByName(String cn_user_name);
+    @Insert("insert into cn_user(cn_user_id,cn_user_name,cn_user_password,cn_user_nick) values(#{cn_user_id},#{cn_user_name},#{cn_user_password},#{cn_user_nick})")
+    void save(User user);
 }
--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -1,14 +1,28 @@
 import com.edu.zzc.dao.UserDao;
+import com.edu.zzc.entity.User;
+import com.edu.zzc.util.NoteUtil;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.transaction.annotation.Transactional;
 
 public class TestCase {
-    @Test
-    public void test1(){
-        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserDao userDao = ac.getBean("userDao", UserDao.class);
-        System.out.println(userDao.findAll());
-    }
+//    @Test
+//    public void test1(){
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        UserDao userDao = ac.getBean("userDao", UserDao.class);
+//        System.out.println(userDao.findAll());
+//    } @Test
+//    public void test2(){
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        UserDao userDao = ac.getBean("userDao", UserDao.class);
+//        System.out.println(userDao.findByName("zhouj"));
+//    }
+//    @Test
+//    public void test1(){
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        UserDao userDao= ac.getBean("userDao",UserDao.class);
+//        User user = new User();
+//        user.setCn_user_id(NoteUtil.createID());
+//    }
 }
